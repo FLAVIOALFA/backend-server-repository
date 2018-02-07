a9201a68400ea2c77f7675b21c1463f57eda0e55
Flavio <fg.alfaro94@gmail.com>
2018-02-07 09:38:28 -0300

    Seccion 12 terminada

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = '@este-es@-un-seed-dificil';
+
+//Google
+module.exports.GOOGLE_CLIENT_ID = '839572636799-oj03v2tc604d9gvbbdg1s5it0fc229r3.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'NcgzOAlhRFTI80pglean1uf9';
--- routes/login.js
+++ routes/login.js
@@ -2,10 +2,111 @@ var express = require('express');
 var app = express();
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
-var SEED = require('../config/config').SEED;
+const SEED = require('../config/config').SEED;
 
 var Usuario = require('../models/usuario');
 
+var GoogleAuth = require('google-auth-library');
+var auth = new GoogleAuth;
+
+const GOOGLE_CLIENT_ID = require('../config/config').GOOGLE_CLIENT_ID;
+const GOOGLE_SECRET = require('../config/config').GOOGLE_SECRET;
+
+//===========================================
+// AUTENTICACION POR GOOGLE
+//===========================================
+
+app.post('/google', (req, res) => {
+
+    var token = req.body.token || 'XXX';
+
+    var client = new auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_SECRET, '');
+
+    client.verifyIdToken(token, GOOGLE_CLIENT_ID, (err, login) => {
+
+        if (err) {
+            return res
+                .status(400)
+                .json({
+                    ok: true,
+                    mensaje: 'Token no válido',
+                    errors: err
+                });
+        }
+
+        var payload = login.getPayload();
+        var userid = payload['sub'];
+        // If request specified a G Suite domain:
+        //var domain = payload['hd'];
+
+        Usuario.findOne({ email: payload.email }, (err, usuario) => {
+            if (err) {
+                return res
+                    .status(500)
+                    .json({
+                        ok: true,
+                        mensaje: 'Error al buscar usuario - login',
+                        errors: err
+                    });
+            }
+
+            if (usuario) {
+                if (usuario.google === false) {
+                    return res
+                        .status(400)
+                        .json({
+                            ok: true,
+                            mensaje: 'Debes autenticarte normalmente',
+                            errors: err
+                        });
+                } else {
+                    // Crear un token!!
+                    usuario.password = ":)";
+                    var token = jwt.sign({ usuario: usuario }, SEED, { expiresIn: 14400 });
+                    res.status(200).json({
+                        ok: true,
+                        usuario: usuario,
+                        token: token,
+                        id: usuario._id
+                    });
+                }
+                //Si el usuario no existe..
+            } else {
+                var newUsuario = new Usuario();
+                newUsuario.nombre = payload.name;
+                newUsuario.email = payload.email;
+                newUsuario.password = ':)';
+                newUsuario.google = true;
+                newUsuario.img = payload.picture;
+
+                newUsuario.save((err, usuarioDB) => {
+                    if (err) {
+                        return res.status(500).json({
+                            ok: false,
+                            mensaje: 'Error al crear usuario - google',
+                            errors: err
+                        })
+                    }
+
+                    var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 });
+                    res.status(200).json({
+                        ok: true,
+                        usuario: usuarioDB,
+                        token: token,
+                        id: usuarioDB._id
+                    });
+
+                });
+            }
+        });
+
+    });
+});
+
+
+//===========================================
+// AUTENTICACION NORMAL
+//===========================================
 app.post('/', (req, res) => {
 
     var body = req.body;
